97963d0f20aa099fc669cbc13696caa753aacb40
arewedazing <june.yongwk@gmail.com>
2022-10-21 02:17:50 +0800

    ahhhh

---
+++ JS/index.js
@@ -0,0 +1,21 @@
+// CART
+function openNav() {
+    document.getElementById("myCart").style.width = "400px";
+}
+
+function closeNav() {
+    document.getElementById("myCart").style.width = "0";
+}
+
+// BACK TO TOP BUTTON in Footer
+var bttop = document.getElementById("bttop")
+            var rootElement = document.documentElement
+
+function BackToTop() {
+    rootElement.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    })
+}
+
+bttop.addEventListener("click", scrollToTop)

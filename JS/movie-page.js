50327fe10758ac2115ea47479af5bb3c74911fe3
arewedazing <june.yongwk@gmail.com>
2022-11-11 23:33:04 +0800

    rg

--- JS/movie-page.js
+++ JS/movie-page.js
@@ -1,12 +1,3 @@
-// CART
-function openNav() {
-    document.getElementById("myCart").style.width = "400px";
-}
-
-function closeNav() {
-    document.getElementById("myCart").style.width = "0";
-}
-
 // BACK TO TOP BUTTON in Footer
 var bttop = document.getElementById("bttop")
             var rootElement = document.documentElement
